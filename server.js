46675227747b211d66704b9bde432bd47419036e
Spike Nox <spike01@gmail.com>
2014-10-19 21:06:08 +0100

    port

--- server.js
+++ server.js
@@ -13,8 +13,9 @@ server.get('/page3', function(request, response){
   response.send('<h1>The Sun is a terrible newspaper</h1>');
 });
 
-http.listen(3000, function(){
-  console.log('listening on port 3000');
+port = process.env.PORT || 3000
+http.listen(port, function(){
+  console.log('listening on port ' + port);
 });
 
 io.on('connection', function (socket) {
